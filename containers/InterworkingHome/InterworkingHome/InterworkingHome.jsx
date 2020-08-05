252eb5e0f9b0c8cdf4d225ace2a85271d70d2694
qiaoshuai <1522740101@qq.com>
2020-08-05 10:50:03 +0800

    'updateRouterandTree'

--- containers/InterworkingHome/InterworkingHome/InterworkingHome.jsx
+++ containers/InterworkingHome/InterworkingHome/InterworkingHome.jsx
@@ -6,24 +6,50 @@ import MineData from '../../../components/MineData/MineData'
 
 import { Input } from 'antd'
 import styles from './InterworkingHome.scss'
+import { Redirect } from 'react-router-dom'
 
 class InterworkingHome extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      isname: 'goMonitoring',
     }
+
+    this.RedirectsRouter()
   }
   componentDidMount = () => {
 
   }
+  componentDidUpdate = () => {
+
+  }
+  RedirectsRouter = () => {
+    if (this.props.location.pathname === '/') {
+      this.props.history.push('interworkingHome/Monitoring')
+    }
+  }
   goMonitoring = () => {
+    this.setState({
+      isname: 'goMonitoring',
+    })
     this.props.history.push('/interworkingHome/Monitoring')
   }
-
+  evaluate = () => {
+    this.setState({
+      isname: 'evaluate',
+    })
+    this.props.history.push('/interworkingHome/Monitoring')
+  }
+  goUser = () => {
+    this.setState({
+      isname: 'goUser',
+    })
+    this.props.history.push('/interworkingHome/Monitoring')
+  }
   render() {
     const { Search } = Input
     console.log(this.props.route.routes, '123456')
+    const { isname } = this.state
     return (
       <div className={styles.InterworkingHomeBox}>
         <Header {...this.props} />
@@ -35,16 +61,22 @@ class InterworkingHome extends Component {
               style={{ width: 230 }}
             />
           </div>
-          <div className={styles.InterworkLeft_Title}>
-            <span onClick={this.goMonitoring}>全局监视</span>
+          <div onClick={this.goMonitoring} className={`${isname === 'goMonitoring' ? styles.active : ''} ${styles.InterworkLeft_Title}`}>
+            <span /><span>全局监视</span>
           </div>
           <CustomTree />
+          <div onClick={this.evaluate} className={`${isname === 'evaluate' ? styles.active : ''} ${styles.InterworkLeft_Title}`}>
+            <span /> <span>仿真评价数据分析</span>
+          </div>
+          <div onClick={this.goUser} className={`${isname === 'goUser' ? styles.active : ''} ${styles.InterworkLeft_Title}`}>
+            <span /><span>用户管理</span>
+          </div>
         </div>
         <div className={styles.Interwork_right}>
           <MineData />
           {renderRoutes(this.props.route.routes)}
         </div>
-      </div>
+      </div >
     )
   }
 }
